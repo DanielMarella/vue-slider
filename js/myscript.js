cda69715dddce526d98b1e87d0f9252bf4585c2b
DanielMarella <marella.daniel96@gmail.com>
2023-06-05 17:20:09 +0200

    break all, idk why

--- js/myscript.js
+++ js/myscript.js
@@ -6,23 +6,37 @@
     createApp({
     data() {
         return {
-        message: 'Which cat are you today?'
-        }
-
         images : [
-            '../img/assassin.jpg'
-            '../img/flycat.jpg'
-            '../img/sherif.jpg'
-            '../img/stylecat.webp'
-        ],
+            './img/assassin.jpg',
+            './img/flycat.jpg',
+            './img/sherif.jpg',
+            './img/stylecat.webp'
+            ],
 
         activeIndex : 0,
-
+        }
     },
 
+    methods: {
+        prevImage(){
+            if ( this.activeIndex === 0){
+                this.activeIndex = this.image.length - 1;
+            } else {
+                this.activeIndex--;
+            }
+        },
+
+        nextImage (){
+            if ( this.activeIndex === this.image.length -1){
+                this.activeIndex = 0;
+            } else{
+                this.activeIndex++;
+            }
+        }
+    }
+
+}).mount('#app');
 
 
 
 
-
-    }).mount('#app')
